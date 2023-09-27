f8824ce0e53c5669cdf4f511d49525de8de5ab47
toolsapkpro <145754947+toolsapkpro@users.noreply.github.com>
2023-09-27 11:31:07 -0700

    Add files via upload

---
+++ d.js
@@ -0,0 +1,7 @@
+function myImg() {
+    alert("Ready to buy Access key now?")
+}
+function pasuser(form) {
+    if (form.pass.value=="lucy23") { location="result.html" }
+    else { alert("Invalid Access key!.. Buy Now!") }
+}
